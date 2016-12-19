324c3c28f39567a3435ba3dd7ddd1faf5c933646
Rafael Pax <rpax@ucm.es>
2016-12-19 11:54:00 +0100

    Removed unused classes

--- components-plugin/src/main/java/com/massisframework/sweethome3d/plugins/components/properties/PropertiesContainer.java
+++
@@ -1,8 +0,0 @@
-package com.massisframework.sweethome3d.plugins.components.properties;
-
-public interface PropertiesContainer {
-
-	public String getProperty(String name);
-
-	public void setProperty(String name, String value);
-}
